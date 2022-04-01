073fdcf939e22825592543d61e0b18e2b7ce00fd
Shaun Inman <shaun@panic.com>
2022-03-31 20:38:31 -0400

    updater can now call a post-update script
    at the point it is called the old progressui is still running (and useable) but the new .system folder is in place

--- skeleton/.system/paks/Update.pak/launch.sh
+++ skeleton/.system/paks/Update.pak/launch.sh
@@ -15,17 +15,26 @@ TOTAL=`unzip -l ${INSTALL_ZIP} | grep -o -E '[0-9]+\s+files$' | grep -o -E '[0-9
 unzip -l $INSTALL_ZIP | awk -F" " '{print $4}' | grep -o -E '.*\/.*' | while read FILE_PATH
 do
 	NUM=`expr $NUM + 1`
-	PERCENT=`expr $NUM \* 97 \/ $TOTAL`
+	PERCENT=`expr $NUM \* 99 \/ $TOTAL`
 	BASENAME=$(basename $FILE_PATH)
 	progress $PERCENT Unzipping $BASENAME
 	unzip -q -o "$INSTALL_ZIP" "$FILE_PATH" -d "$TMP_UPDATE_DIR" 
 done
 
+progress 99 Cleaning up
+sleep 1
+
 mv "$SYSTEM_PATH" "$TMP_UPDATE_DIR/.system-old"
 mv "$TMP_UPDATE_DIR/.system" "$SYSTEM_PATH"
+rm -f "$INSTALL_ZIP"
+
+# perform post-update actions if present in updated .system folder
+if [ -f "$SYSTEM_PATH/post-update.sh" ]; then
+	"$SYSTEM_PATH/post-update.sh"
+	rm -f "$SYSTEM_PATH/post-update.sh"
+fi
 
 progress 100 Update complete
-rm -f "$INSTALL_ZIP"
 sleep 1
 
 progress quit
