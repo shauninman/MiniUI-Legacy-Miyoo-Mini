77279e20b4c18c813cc969cf836a4bc7107cd7af
Shaun Inman <shaun@panic.com>
2022-08-13 16:28:32 -0400

    added Super Game Boy and Game Boy Advance extras
    both use mgba and can take advantage of collated folders

---
+++ extras/Emus/MGBA.pak/launch.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+EMU_EXE=mgba
+
+###############################
+
+EMU_TAG=$(basename "$(dirname "$0")" .pak)
+ROM="$1"
+mkdir -p "$BIOS_PATH/$EMU_TAG"
+mkdir -p "$SAVES_PATH/$EMU_TAG"
+HOME="$USERDATA_PATH"
+cd "$HOME"
+picoarch "$CORES_PATH/${EMU_EXE}_libretro.so" "$ROM" DMG &> "$LOGS_PATH/$EMU_TAG.txt"
