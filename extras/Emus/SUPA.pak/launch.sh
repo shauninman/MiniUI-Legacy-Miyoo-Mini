b3f35f690b416d6717bf261bedee7666460cf99e
Shaun Inman <shaun@panic.com>
2022-08-13 16:17:05 -0400

    renamed extras SFC.pak to SUPA.pak
    for use with collated folders

--- extras/Emus/SUPA.pak/launch.sh
+++ extras/Emus/SUPA.pak/launch.sh
@@ -6,15 +6,6 @@ CORES_PATH=$(dirname "$0")
 ###############################
 
 EMU_TAG=$(basename "$(dirname "$0")" .pak)
-#------------------------------
-# update from unprefixed name
-OLD_EXE=supafaust
-OLD_DATA_PATH="$USERDATA_PATH/.picoarch-${OLD_EXE}-${EMU_TAG}"
-if [ -d "$OLD_DATA_PATH" ]; then
-	NEW_DATA_PATH="$USERDATA_PATH/.picoarch-${EMU_EXE}-${EMU_TAG}"
-	mv "$OLD_DATA_PATH" "$NEW_DATA_PATH"
-fi
-#------------------------------
 ROM="$1"
 mkdir -p "$BIOS_PATH/$EMU_TAG"
 mkdir -p "$SAVES_PATH/$EMU_TAG"
